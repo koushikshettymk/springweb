a595658c3bf67a0fdbc8f9470d9f221c7a353103
KoushikMK <shettykoushik@gmail.com>
2024-08-19 20:52:45 +0530

    my first project

---
+++ src/main/java/com/learnspringapp/springwepbapp/SpringwepbappApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringapp.springwepbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwepbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwepbappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspringapp/springwepbapp/SpringwepbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringapp.springwepbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwepbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
